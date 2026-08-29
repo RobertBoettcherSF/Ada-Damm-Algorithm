61a5d92955ad18a24b46bb1807411aa8802e9c51
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-29 22:22:56 +0200

    Refactor main.adb to improve validation output

--- main.adb
+++ main.adb
@@ -1,5 +1,5 @@
--- main.adb
--- A simple entry point to demonstrate the core usage of the algorithm.
+--  main.adb
+--  A simple entry point to demonstrate the core usage of the algorithm.
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Damm_Algorithm; use Damm_Algorithm;
@@ -7,10 +7,13 @@ with Damm_Algorithm; use Damm_Algorithm;
 procedure Main is
    Test_Str : constant String := "572";
    Check    : Digit;
+   Is_Valid : Boolean;
 begin
    Put_Line ("=== Damm Algorithm Demonstration ===");
    Check := Calculate_Check_Digit_Str (Test_Str);
    Put_Line ("Input Number: " & Test_Str);
    Put_Line ("Calculated Check Digit: " & Check'Image);
-   Put_Line ("Verification of 5724: " & Boolean'Image (Verify_Number_Str ("5724")));
+   
+   Is_Valid := Verify_Number_Str ("5724");
+   Put_Line ("Verification of 5724: " & Boolean'Image (Is_Valid));
 end Main;
